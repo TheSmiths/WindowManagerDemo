be9ba28eb0824f79a65be088eb27ec40d7677015
Duy Bao Nguyen <bduyng@gmail.com>
2015-08-07 03:13:02 +1000

    clean up commented code

--- app/controllers/requires/index.js
+++ app/controllers/requires/index.js
@@ -7,21 +7,21 @@
     }
 })(arguments[0] || {});
 
-function selectedDemo(e) {
+function selectedModule(e) {
     if (!e.row.type) return false;
 
-    var libraries = {
+    var modules = {
         'drawer' : {
-            controller : 'drawer',
+            filename : 'drawer',
             src : 'DrawerManager.2.1.2'
         },
         'tabs' : {
-            controller : 'tabs',
+            filename : 'tabs',
             src : 'TabsManager.0.9.2'
         }
     };
 
-    return Alloy.createController(libraries[e.row.type].controller, {
-        windowManager : require(libraries[e.row.type].src)
+    return Alloy.createController(modules[e.row.type].filename, {
+        windowManager : require(modules[e.row.type].src)
     }).open();
 }
